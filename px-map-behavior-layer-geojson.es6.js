5aaeb3975dfb3bf40e26e88819cafe158f013274
William Chang <william.chang@ge.com>
2017-06-15 12:22:36 +0100

    Only create editTools on map once

--- px-map-behavior-layer-geojson.es6.js
+++ px-map-behavior-layer-geojson.es6.js
@@ -153,8 +153,11 @@
         }
       });
       if(this.editable) {
-        this.parentNode.elementInst.editTools = new L.Editable(this.parentNode.elementInst, {featuresLayer: geojsonLayer});
-        //Disable doubleclick zoom when drawing to prevent zooming when double clicking to end a line
+        if (!this.parentNode.elementInst.editTools) {
+          this.parentNode.elementInst.editTools = new L.Editable(this.parentNode.elementInst, {featuresLayer: geojsonLayer});
+        } else {
+          this.parentNode.elementInst.editTools.featuresLayer.addLayer(geojsonLayer);
+        }        //Disable doubleclick zoom when drawing to prevent zooming when double clicking to end a line
         this.parentNode.elementInst.editTools.addEventListener('editable:drawing:start', () => {
           this.parentNode.elementInst.doubleClickZoom.disable();
         });
